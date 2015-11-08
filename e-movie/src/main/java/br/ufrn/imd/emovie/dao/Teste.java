b7abc4b0e85a0cd60f4ebd35fb63ac9c45b64848
João Eduardo Medeiros <joaoribeiromedeiros@gmail.com>
2015-11-08 12:02:55 -0300

    Log4j configurado. Por enquanto usando o BasicConfigurator.

--- e-movie/src/main/java/br/ufrn/imd/emovie/dao/Teste.java
+++ e-movie/src/main/java/br/ufrn/imd/emovie/dao/Teste.java
@@ -3,11 +3,8 @@
  */
 package br.ufrn.imd.emovie.dao;
 
-import java.util.Date;
-
-import br.ufrn.imd.emovie.model.Movie;
-import br.ufrn.imd.emovie.model.Room;
-import br.ufrn.imd.emovie.model.Session;
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
 
 /**
  * @author joao
@@ -19,22 +16,9 @@ public class Teste {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		DaoMovie dao = new UserDao();
-		
-		Movie movie = new Movie("Ghost Busters", new Date(), new Date());
-		dao.create(movie);
-		System.out.println(dao.getAll(Movie.class));
-		
-		Room room1 = new Room(100);
-		Room room2 = new Room(150);
-		Room room3 = new Room(200);
-		Room room4 = new Room(50);
-		
-		Session session1 = new Session(0,1);
-		Session session2 = new Session(0,2);
-		Session session3 = new Session(3,1);
-		Session session4 = new Session(3,2);
-		
+		Logger logger = Logger.getLogger(Teste.class);
+		BasicConfigurator.configure();
+		logger.debug("Teste Log4j.");
 	}
 
 }
