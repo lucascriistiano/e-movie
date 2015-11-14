25a6b0df4f47dead3c61f21fef6598a952ccc8f9
João Eduardo Medeiros <joaoribeiromedeiros@gmail.com>
2015-11-14 00:33:44 -0300

    Rodapé simplificado

--- site/admin/footer.php
+++ site/admin/footer.php
@@ -2,71 +2,11 @@
 <footer>
 	<div class="wrapper cf">
 
-		<!-- widgets -->
-		<ul  class="widget-cols cf">
-			<li class="first-col">
-
-				<div class="widget-block">
-					<h4> </h4>
-					<div class="recent-post cf">
-						<a href="#" class="thumb"><img src="img/dummies/54x54.gif" alt="Post" /></a>
-						<div class="post-head">
-							<a href="#">Filme Vingadores</a><span> 10/10/2015</span>
-						</div>
-					</div>
-					<div class="recent-post cf">
-						<a href="#" class="thumb"><img src="img/dummies/54x54.gif" alt="Post" /></a>
-						<div class="post-head">
-							<a href="#">Filme Wolverine</a><span> 08/10/2015</span>
-						</div>
-					</div>
-					<div class="recent-post cf">
-						<a href="#" class="thumb"><img src="img/dummies/54x54.gif" alt="Post" /></a>
-						<div class="post-head">
-							<a href="#">Filme Smurfs</a><span> 07/10/2015</span>
-						</div>
-					</div>
-				</div>
-			</li>
-
-			<li class="second-col">
-
-				<div class="widget-block">
-					<h4> </h4>
-					<p>Somos o melhor e mais antigo cinema da cidade. </p>
-				</div>
-
-			</li>
-
-			<li class="third-col">
-				<div class="widget-block">
-					<div id="tweets" class="footer-col tweet">
-						<h4>TWITTER</h4>
-					</div>
-				</div>
-			</li>
-
-			<li class="fourth-col">
-				<div class="widget-block">
-					<h4> </h4>
-					<ul>
-						<li class="cat-item"><a href="#" >Filmes</a></li>
-						<li class="cat-item"><a href="#" >Horários</a></li>
-					</ul>
-				</div>
-
-			</li>
-		</ul>
-		<!-- ENDS widgets -->
-
-
 		<!-- bottom -->
 		<div class="footer-bottom">
 			<div class="left">Template por <a href="http://luiszuno.com" >luiszuno.com</a></div>
 			<ul id="social-bar" class="cf sb">
-				<li><a href="http://www.facebook.com"  title="Become a fan" class="facebook">Facebbok</a></li>
-				<li><a href="http://www.twitter.com" title="Follow my tweets" class="twitter"></a></li>
-				<li><a href="http://plus.google.com" title="Enter my circles" class="plus"></a></li>
+				&copy; UFRN - 2015
 			</ul>
 		</div>
 		<!-- ENDS bottom -->
